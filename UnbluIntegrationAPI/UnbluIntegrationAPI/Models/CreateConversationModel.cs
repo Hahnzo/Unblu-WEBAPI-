7a9e02f2826e27903eec79d7f462a983940d0e67
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2024-09-06 16:44:02 -0400

    Final Update

--- UnbluIntegrationAPI/UnbluIntegrationAPI/Models/CreateConversationModel.cs
+++ UnbluIntegrationAPI/UnbluIntegrationAPI/Models/CreateConversationModel.cs
@@ -4,6 +4,8 @@ namespace UnbluIntegrationAPI.Models
 {
     public class CreateConversationModel
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "ConversationCreationModel";
         public string Topic { get; set; }
         public Recipient Recipient { get; set; }
         public List<Participant> Participants { get; set; }
@@ -11,22 +13,27 @@ namespace UnbluIntegrationAPI.Models
         public string ConversationVisibility { get; set; }
         public string Locale { get; set; }
         public string VisitorData { get; set; }
-        public string ConversationTemplateId { get; set; }
+        public string? ConversationTemplateId { get; set; }
         public bool InheritConfigurationAndTexts { get; set; }
-        public string ExternalMessengerChannelId { get; set; }
-        public string SourceId { get; set; }
-        public string SourceUrl { get; set; }
+
+        public string? SourceId { get; set; }
+        public string? SourceUrl { get; set; }
         public string InitialEngagementUrl { get; set; }
         public Metadata Metadata { get; set; }
     }
 
     public class Recipient
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "ConversationCreationRecipientData";
         public string Id { get; set; }
     }
 
     public class Participant
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "ConversationCreationParticipantData"; 
+
         public string PersonId { get; set; }
         public bool Hidden { get; set; }
         public bool ConversationStarred { get; set; }
--- UnbluIntegrationAPI/UnbluIntegrationAPI/Models/InvitationSearchModel.cs
+++ UnbluIntegrationAPI/UnbluIntegrationAPI/Models/InvitationSearchModel.cs
@@ -4,23 +4,35 @@ namespace UnbluIntegrationAPI.Models
 {
     public class Operator
     {
-       public string value { get; set; }
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "EqualsIdOperator";
+
+        [JsonProperty("type")]
+        public string ComparisonType { get; set; } = "EQUALS";
+        public string value { get; set; }
     }
 
     public class OrderBy
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "ConversationInvitationOrderBy";
         public string field { get; set; }
         public string order { get; set; }
     }
 
     public class InvitationSearchModel
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "ConversationInvitationQuery";
         public List<SearchFilter> searchFilters { get; set; }
         public List<OrderBy> orderBy { get; set; }
     }
 
     public class SearchFilter
     {
+        [JsonProperty("$_type")]
+        public string Type { get; set; } = "TargetTypeConversationInvitationSearchFilter";
+
         public string field { get; set; }
         public Operator @operator { get; set; }
     }
